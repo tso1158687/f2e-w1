d8eadd8336d9e3137e678f1c525a13e66eec4b40
jason zheng <101104044@nccu.edu.tw>
2018-06-04 23:36:33 +0800

    generate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyTasksComponent } from './my-tasks/my-tasks.component';
+import { InProgressComponent } from './in-progress/in-progress.component';
+import { CompletedComponent } from './completed/completed.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTasksComponent,
+    InProgressComponent,
+    CompletedComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
